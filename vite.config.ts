30578c6f607afc3b44aeb990f46c08a83759ca42
Coke And Code <kevin@rune.ai>
2024-10-06 11:39:34 +0100

    Update to Rune

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 import { qrcode } from "vite-plugin-qrcode"
-import rune from "vite-plugin-dusk"
+import rune from "vite-plugin-rune"
 import path from "node:path"
 
 // https://vitejs.dev/config/
